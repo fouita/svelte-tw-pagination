229f2cc2163c31ad558fa46fb2fb4ed84f666c09
abdelhay <najjar.abdelhay@gmail.com>
2021-04-07 10:16:15 +0100

    fix page num

--- src/Pagination.svelte
+++ src/Pagination.svelte
@@ -13,7 +13,7 @@
   export let color = "indigo";
   $: s = small ? 8 : 12;
 
-  $: num_pages = Math.floor(num_items / per_page);
+  $: num_pages = Math.ceil(num_items / per_page);
 
   let arr_pages = [];
 
@@ -43,7 +43,7 @@
   }
 
   $: if (num_items) {
-    num_pages = Math.round(num_items / per_page);
+    num_pages = Math.ceil(num_items / per_page);
     current = current || 1;
   }
 
